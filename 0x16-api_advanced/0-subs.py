1d2c57079eb04bfb9d1c0e0edbce467b07c61cc2
Milla2000 <millajesso2000@gmail.com>
2024-08-13 22:44:48 +0300

    API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+Module that queries the Reddit API and returns the number of subscribers
+for a given subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+    """
+    Function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit. If the subreddit is invalid,
+    returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyBot/0.0.1'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
         data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+        return data['data'].get('subscribers', 0)
+
+    except requests.exceptions.RequestException:
         return 0
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers("programming"))
+    print(number_of_subscribers("this_is_a_fake_subreddit"))
